f558f017f31a0f1679a70dbb2c175b626199178b
tazo01~ <nadiradzetazo123@gmail.com>
2023-11-26 23:02:50 +0400

    fetch data from api

--- website/src/features/products/beautyproducts/beauty.jsx
+++ website/src/features/products/beautyproducts/beauty.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
 export default function Beauty() {
+  const [data, setData] = useState([]);
+  console.log(data);
   useEffect(() => {
     axios
       .get("http://makeup-api.herokuapp.com/api/v1/products.json")
-      .then((res) => console.log(res.data))
+      .then((res) => setData(res.data))
       .catch((err) => console.log(err));
   }, []);
-  return <div>this is beauty page</div>;
+  return (
+    <div>
+      {data.map((each) => {
+        return each.name;
+      })}
+    </div>
+  );
 }
